f2e290066d24eadfac59cb2381e369e6af1d4dea
JC Zaragoza <jzara3115@gmail.com>
2025-02-05 16:14:00 -0800

    starting of auto

--- src/autoHTN.py
+++ src/autoHTN.py
@@ -15,41 +15,73 @@ def produce (state, ID, item):
 
 pyhop.declare_methods ('produce', produce)
 
-def make_method (name, rule):
-	def method (state, ID):
-		# your code here
-		pass
-
-	return method
+def make_method(name, rule):
+    def method(state, ID):
+        subtasks = []
+        if 'Requires' in rule:
+            for req, num in rule['Requires'].items():
+                subtasks.append(('have_enough', ID, req, num))
+        if 'Consumes' in rule:
+            for cons, num in rule['Consumes'].items():
+                subtasks.append(('have_enough', ID, cons, num))
+        subtasks.append(('produce_{}'.format(name), ID))
+        return subtasks
+    return method
 
 def declare_methods (data):
 	# some recipes are faster than others for the same product even though they might require extra tools
 	# sort the recipes so that faster recipes go first
+			for recipe_name, recipe in data['Recipes'].items():
+				method = make_method(recipe_name, recipe)
+				pyhop.declare_methods('produce_{}'.format(recipe_name), method)
+	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)		
 
-	# your code here
-	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)	
-	pass			
+def make_operator(rule):
+    def operator(state, ID):
+        if 'Requires' in rule:
+            for req, num in rule['Requires'].items():
+                if getattr(state, req)[ID] < num:
+                    return False
+        if 'Consumes' in rule:
+            for cons, num in rule['Consumes'].items():
+                if getattr(state, cons)[ID] < num:
+                    return False
+        if 'Consumes' in rule:
+            for cons, num in rule['Consumes'].items():
+                setattr(state, cons, {ID: getattr(state, cons)[ID] - num})
+        if 'Produces' in rule:
+            for prod, num in rule['Produces'].items():
+                setattr(state, prod, {ID: getattr(state, prod)[ID] + num})
+        state.time[ID] += rule['Time']
+        return state
+    return operator
 
-def make_operator (rule):
-	def operator (state, ID):
-		# your code here
-		pass
-	return operator
-
-def declare_operators (data):
+def declare_operators(data):
+    operators = []
+    for recipe_name, recipe in data['Recipes'].items():
+        operator = make_operator(recipe)
+        operators.append(operator)
+    pyhop.declare_operators(*operators)
 	# your code here
 	# hint: call make_operator, then declare the operator to pyhop using pyhop.declare_operators(o1, o2, ..., ok)
-	pass
 
 def add_heuristic (data, ID):
 	# prune search branch if heuristic() returns True
 	# do not change parameters to heuristic(), but can add more heuristic functions with the same parameters: 
 	# e.g. def heuristic2(...); pyhop.add_check(heuristic2)
-	def heuristic (state, curr_task, tasks, plan, depth, calling_stack):
-		# your code here
-		return False # if True, prune this branch
+    def heuristic (state, curr_task, tasks, plan, depth, calling_stack):
+        if state.time[ID] > 300:
+            return True
+        return False # if True, prune this branch
 
-	pyhop.add_check(heuristic)
+    def no_infinite_regression(state, curr_task, tasks, plan, depth, calling_stack):
+        # Check if the current task is already in the plan
+        if curr_task in plan:
+            return True
+        return False
+
+    pyhop.add_check(heuristic)
+    pyhop.add_check(no_infinite_regression)
 
 
 def set_up_state (data, ID, time=0):
